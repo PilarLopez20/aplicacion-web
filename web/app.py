486d17d5ba9b9832a7eac2d49215d8b0cf53c08d
Pilar Lopez <78154372+PilarLopez20@users.noreply.github.com>
2024-10-15 14:45:50 -0600

    Update app.py

--- web/app.py
+++ web/app.py
@@ -1,1 +1,16 @@
+import web
 
+urls = (
+    '/(.*)', 'hello',
+  
+)
+app = web.application(urls, globals())
+
+class hello:
+    def GET(self, name):
+        if not name:
+            name = 'by Pilar'
+        return 'Hello docker ' + name + '!'
+
+if __name__ == "__main__":
+    app.run()
